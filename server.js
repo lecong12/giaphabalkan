2d68e61ff3ad75f20c0234fd0d86896267c831ca
lecong12Home <lecong12@gmail.com>
2026-01-25 17:56:58 +0700

    update

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server đang chạy trên port ${port}`);
 });
